fb681fea037fb2d9cefed912e98c03c88ca7a490
Ayush Tripathi <707ayushtripathi@gmail.com>
2024-06-17 03:05:36 +0530

    init

---
+++ ep13.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=1e5+10;
+int a[N];
+int pf[N];
+
+int main()
+{
+	int n;
+	cin>>n;
+	for (int i = 1; i <=n; ++i)
+	{
+		cin>>a[i];
+		pf[i]=pf[i-1]+a[i];
+	}
+	int q;
+	cin>>q;
+	while(q--){
+		int l,r;
+		cin>>l>>r;
+		long long sum=pf[r]-pf[l-1];
+		cout<<sum<<endl;
+	}
+	
+} 
---
+++ ep16.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=1e5 +10;
+int hsh[N][26];
+int main()
+{
+	
+	int n,q;
+	cin>>n>>q;
+	string n;
+	cin>>n;
+	
+}
+//to check whether a string is palindrome after rearranging
+//for q queries from index l to r
